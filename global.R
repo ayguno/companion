1f6d7da29c84b5d85907b7af7b4606133b236c31
ayguno <ozan@broadinstitute.org>
2017-12-31 14:05:50 -0500

    Most errors in first tab are fixed

--- global.R
+++ global.R
@@ -130,10 +130,10 @@ queryDeviceData <- function(drug,
         companions <- unique(drug.device.table$Device.Trade.Name[which(drug.device.table$Drug.Trade.Name == tolower(drug))])
         
         device.event.data <- data.frame(device = "",
-                                        date_of_event = "",
+                                        date_of_event = as.Date("2000-01-01"),
                                         event_type = "",
                                         event_location ="",
-                                        event_description = "")
+                                        event_description = "", stringsAsFactors = FALSE)
         
         for(i in seq_along(companions)){
                 
@@ -148,6 +148,46 @@ queryDeviceData <- function(drug,
                                            event_location = device.event.query$event_location,
                                            event_description = event.text.vector)
                         
+                        # Convert all features to chr:
+                        for(j in 1:ncol(temp)){
+                                temp[,j] <- as.character(temp[,j])
+                        }
+                        
+                        
+                        # Convert the date feature to proper date
+                        for(k in 1:nrow(temp)){
+                                year <- substr(temp$date_of_event[k],1,4)
+                                month <- substr(temp$date_of_event[k],5,6)
+                                day <- substr(temp$date_of_event[k],7,8)
+                                ymd.date <- paste(year,month,day,sep = "-")
+                                temp$date_of_event[k] <- ymd.date
+                        }
+                        
+                        temp$date_of_event <- date(format(temp$date_of_event))
+
+                        # Add html breaks (<br>) so that description text looks nicer
+                        add.line.breaks <- function(text){
+                                # adds a text break after each 10 words
+                                text <-  unlist(strsplit(text,split = " "))
+                                text.temp <- ""
+                                start.word <- 1
+                                end.word <-10
+                                while(end.word < length(text)){
+                                        text.temp.temp <- text[start.word:end.word]
+                                        text.temp <- c(text.temp,text.temp.temp,"<br>")
+                                        start.word <- start.word + 10
+                                        end.word <- end.word + 10
+                                }
+
+                                # Combine them back to one sentence
+                                text <- paste(text.temp, collapse = " ")
+                                
+                        }# End of  add.line.breaks function
+                                
+                                # Call add.line.breaks to add breaks to text
+                                temp$event_description <- sapply(as.character(temp$event_description),add.line.breaks)
+                        
+                        # Aggregate data                
                         device.event.data <- rbind(device.event.data,temp) 
                         
                 }       
@@ -160,13 +200,55 @@ queryDeviceData <- function(drug,
         }else{
                 device.event.data <- device.event.data[-1,] # remove the first row
                 
-                # Convert all features to chr:
-                for(j in 1:ncol(device.event.data)){
-                        device.event.data[,j] <- as.character(device.event.data[,j])
-                }
-        
-                 
                 return(device.event.data)
         }
         
 } # End of queryDeviceData
+
+
+
+# Multiple plot function
+# from: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+        library(grid)
+        
+        # Make a list from the ... arguments and plotlist
+        plots <- c(list(...), plotlist)
+        
+        numPlots = length(plots)
+        
+        # If layout is NULL, then use 'cols' to determine layout
+        if (is.null(layout)) {
+                # Make the panel
+                # ncol: Number of columns of plots
+                # nrow: Number of rows needed, calculated from # of cols
+                layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                                 ncol = cols, nrow = ceiling(numPlots/cols))
+        }
+        
+        if (numPlots==1) {
+                print(plots[[1]])
+                
+        } else {
+                # Set up the page
+                grid.newpage()
+                pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+                
+                # Make each plot, in the correct location
+                for (i in 1:numPlots) {
+                        # Get the i,j matrix positions of the regions that contain this subplot
+                        matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+                        
+                        print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                                        layout.pos.col = matchidx$col))
+                }
+        }
+}
